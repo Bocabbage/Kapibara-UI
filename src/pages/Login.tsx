f2b35c674f2558caa3f0fabe2c19840fa9580f98
Bocabbage <378066505@qq.com>
2024-01-31 00:31:45 +0800

    Add kapibara-public ui sources.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -33,14 +33,14 @@ export default function Login() {
         // 2. Handle token-expired
         <>
         <title>Kapibara Login</title>
-        <div className="flex min-w-full min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="w-96 bg-teal-50 flex min-w-full min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
-                className="mx-auto h-10 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                className="mx-auto h-48 w-auto"
+                src="/kapibara-maru.png"
             />
             <h2 className="font-oswald-bold mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+                Kapibara System
             </h2>
           </div>
 
@@ -49,7 +49,7 @@ export default function Login() {
               
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="email" className="block text-sm font-oswald-regular leading-6 text-gray-900">
+                  <label htmlFor="email" className="block text-lg font-oswald-regular leading-6 text-gray-900">
                     Account
                   </label>
                 </div>
@@ -66,7 +66,7 @@ export default function Login() {
 
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-oswald-regular leading-6 text-gray-900">
+                  <label htmlFor="password" className="block text-lg font-oswald-regular leading-6 text-gray-900">
                     Password
                   </label>
                 </div>
@@ -85,7 +85,7 @@ export default function Login() {
               <button
                 type="submit"
                 disabled={loading}
-                className="login-button flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-oswald-bold leading-6 text-white shadow-sm hover:bg-emerald-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="outline outline-3 outline-orange-200 login-button flex w-full justify-center rounded-md bg-orange-300 px-3 py-1.5 text-sm font-oswald-bold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
